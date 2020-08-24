361846e91d3da383250b0bb753bbbb29d28f7777
Damien de Mijolla <ddm@splinter-login.local>
2020-08-24 15:18:05 +0100

    made the datasets modular where user specifies what outputs to be returned

--- apoNN/src/datasets.py
+++ apoNN/src/datasets.py
@@ -15,14 +15,17 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 
+
+
 class ApogeeDataset(Dataset):
-    def __init__(self,allStar):
+    def __init__(self,allStar,outputs = None):
         """
         allStar: 
-                an allStar shape array containg those stars chosen for the dataset
+            an allStar shape array containg those stars chosen for the dataset
+        iterables:
+            a list specifying what items to return for dataset calls
         """
         self.allStar = allStar
-        self.return_mask = False
         self.filtered_bits =  [bitmask.apogee_pixmask_int('BADPIX'),
          bitmask.apogee_pixmask_int('CRPIX'),
          bitmask.apogee_pixmask_int('SATPIX'),
@@ -32,12 +35,22 @@ class ApogeeDataset(Dataset):
          bitmask.apogee_pixmask_int('BADFLAT'),
          bitmask.apogee_pixmask_int('BADFLAT'),
          bitmask.apogee_pixmask_int('LITTROW_GHOST')]
+        if outputs is None:
+            self.outputs = ["aspcap","physical","idx"]
+        else: 
+            self.outputs = outputs
         
     def idx_to_prop(self,idx):
         return self.allStar[idx]["APOGEE_ID"],self.allStar[idx]["FIELD"], self.allStar[idx]["TELESCOPE"]
     
     def get_pseudonormalized(self,apogee_id,loc,telescope):
         return apread.aspcapStar(loc_id=str(loc),apogee_id=apogee_id,telescope=telescope,ext=1)
+    
+    def get_apstar(self,apogee_id,loc,telescope):
+        spec,_ = apread.apStar(loc_id=str(loc),apogee_id=apogee_id,telescope=telescope,ext=1)
+        if len(spec.shape)==2:
+            spec = spec[0] #unclear whether I should be operating on 0
+        return spec
     
     def get_mask(self,apogee_id,loc,telescope):
         return apread.apStar(loc_id=str(loc),apogee_id=apogee_id,telescope=telescope,ext=3)[0]
@@ -60,18 +73,38 @@ class ApogeeDataset(Dataset):
     def __len__(self):
         return len(self.allStar)
     
+    
     def __getitem__(self,idx):
         apogee_id,loc,telescope = self.idx_to_prop(idx)
-        spec,hed = self.get_pseudonormalized(apogee_id,loc,telescope)
-        physical_params = self.idx_to_physical(idx)
-        if self.return_mask:
-            masks = self.get_mask(apogee_id,loc,telescope)
-            if len(masks.shape)==2:
-                mask = masks[0] #unclear whether I should be operating on 0
-            else:
-                mask = masks
-            bool_mask = self.filter_mask(mask,self.filtered_bits)
-            return torch.tensor(spec.astype(np.float32)),torch.tensor(bool_mask),physical_params,idx
-        else:
-            return torch.tensor(spec.astype(np.float32)),physical_params,idx
-        
+        returned = []
+        for item in self.outputs:
+            
+            if item == "aspcap":
+                spec,hed = self.get_pseudonormalized(apogee_id,loc,telescope)
+                returned.append(torch.tensor(spec.astype(np.float32)))
+                
+            elif item == "physical":
+                physical_params = self.idx_to_physical(idx)
+                returned.append(physical_params)
+                
+            elif item == "idx":
+                returned.append(idx)
+            
+            elif item == "mask":
+                masks = self.get_mask(apogee_id,loc,telescope)
+                if len(masks.shape)==2:
+                    mask = masks[0] #unclear whether I should be operating on 0
+                else:
+                    mask = masks
+                bool_mask = self.filter_mask(mask,self.filtered_bits)
+                returned.append(torch.tensor(bool_mask))
+            
+            elif item == "apstar":
+                spec = self.get_apstar(apogee_id,loc,telescope)
+                returned.append(torch.tensor(spec.astype(np.float32))/250)
+                            
+            else: 
+                raise Exception(f"{item} is not a valid iterable")
+                
+        return tuple(returned)
+                
