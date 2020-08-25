9fd7d38653214c468dd62992a369557716af3d54
Damien de Mijolla <ddm@splinter-login.local>
2020-08-25 13:50:55 +0100

    added scripts for downloading the apstar and aspcap datastes

---
+++ apoNN/scripts/download_apscap.py
@@ -0,0 +1,57 @@
+import apogee.tools.read as apread
+import matplotlib.pyplot as plt
+import apogee.tools.path as apogee_path
+from apogee.tools import bitmask
+
+import random
+import numpy as np
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+import torch.nn as nn
+
+from apoNN.src.datasets import ApogeeDataset
+
+from tagging.src.networks import ConditioningAutoencoder,Embedding_Decoder,Feedforward,ParallelDecoder,Autoencoder
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+apogee_path.change_dr(16)
+
+
+allStar= apread.allStar(rmcommissioning=True,main=False,ak=True, akvers='targ',adddist=False)
+
+
+upper_temp_cut = allStar["Teff"]<5000
+lower_temp_cut = allStar["Teff"]>4000
+lower_g_cut = allStar["logg"]>1.5
+upper_g_cut = allStar["logg"]<3
+snr_cut = allStar["SNR"]>100
+snr_highcut = allStar["SNR"]<500
+feh_outliercut = allStar["Fe_H"]>-5
+o_outliercut = allStar["O_FE"]>-5
+c_outliercut = allStar["C_FE"]>-5
+na_outliercut = allStar["Na_FE"]>-5
+mg_outliercut = allStar["Mg_FE"]>-5
+si_outliercut = allStar["Si_FE"]>-5
+
+
+
+combined_cut = lower_g_cut & upper_g_cut & lower_temp_cut & upper_temp_cut & snr_cut & snr_highcut & feh_outliercut & o_outliercut &  c_outliercut & na_outliercut & mg_outliercut & si_outliercut
+
+cut_allStar = allStar[combined_cut]
+
+
+
+n_datapoints = 25000
+dataset = ApogeeDataset(cut_allStar[:n_datapoints],outputs = ["aspcap","physical","idx"])
+
+
+for i in range(25000):
+    print(i)
+    dataset[i][1]
+
+
+
+
+
