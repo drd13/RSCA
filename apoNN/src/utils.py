170b5cc0ad7e0598f9be2dc3ce73264d7aff5825
Damien de Mijolla <ddm@splinter-login.local>
2020-10-12 21:44:51 +0100

    adds functions for returning indexes of windows of elements of interest

--- apoNN/src/utils.py
+++ apoNN/src/utils.py
@@ -1,7 +1,12 @@
 import torch
 import torchvision
 import pickle
+import numpy as np
 from pathlib import Path
+import apogee.tools.path as apath
+import apogee.spec.window as apwindow
+
+
 
 def generate_loss_with_masking(loss):
     def loss_with_masking(x_pred,x_true,mask):
@@ -24,4 +29,19 @@ def load(filename):
 
 
 
+def get_window(elem):
+    current_apogee_redux = apath._APOGEE_REDUX
+    apath.change_dr(12)
+    start,end = apwindow.waveregions(elem,asIndex=True)
+    apath._APOGEE_REDUX = current_apogee_redux
+    return (start,end)
 
+
+def get_mask_elem(elem,trimmed=0):
+    """trimmed: number of indexes to trim off of each windows edges"""
+    spec_mask =np.zeros(8575)
+    start,end = get_window(elem)
+    for i in range(len(start)):
+        line_idx = np.arange(start[i],end[i])
+        spec_mask[line_idx] = 1 
+    return spec_mask    
