7467290096be354c05992b913565a12f5c0cfc04
Damien de Mijolla <ddm@splinter-login.local>
2020-10-28 09:45:50 +0000

    adds plotting functions for intracluster and intercluster distances

--- apoNN/src/plotting.py
+++ apoNN/src/plotting.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
+import apoNN.src.vectors as vector
 import numpy as np
-
+import random
 
 
 def summarize_representation(z_all, z_occam, percentage_all=0.1, percentage_occam=0.1,plot_cut=True):
@@ -38,4 +39,77 @@ def summarize_representation(z_all, z_occam, percentage_all=0.1, percentage_occa
         print("here")
         plt.hist(sorted_occam,alpha=0.2,label="diff",bins=20,density=True)
         plt.hist(sorted_all,alpha=0.2,label="full",bins=20, density=True)
+
+
+
+def get_combinations(len_cluster):
+    """
+    Given a cluster size returns a list containing every pair of index (without repeat) 
+    """
+    combinations = []
+    for idx1 in np.arange(len_cluster):
+        for idx2 in np.delete(np.arange(len_cluster),idx1):
+            if sorted([idx1,idx2]) not in combinations:
+                combinations.append(sorted([idx1,idx2]))
+    return combinations
+
+
+
+def get_intracluster_distances(z,z_occam,leave_out=True):
+    """Measures intracluster distances (between stars in the same cluster) after fitting and transforming.
+    INPUTS
+    ------
+    z: apoNN.vector.Vector
+        A vector containing the field star dataset
+    z_occam: vector.OccamLatentVector
+        A vector containing the occam cluster stars
+    leave_out: Boolean
+        True corresponds to excluding clusters being evaluated from training so as to avoid overfitting 
+    """
+    distances = []
+    for cluster in list(z_occam.registry.keys()):
+        if leave_out is True:
+            fitter = vector.Fitter(z,z_occam.without(cluster))
+        else:
+            fitter = vector.Fitter(z,z_occam)
+        v_centered_occam = fitter.transform(z_occam.centered.only(cluster))
+        combinations = get_combinations(len(v_centered_occam))
+        distances_cluster = []
+        for combination in combinations:
+            distances_cluster.append(np.linalg.norm(v_centered_occam[combination[0]]-v_centered_occam[combination[1]]))
+
+        distances.append(distances_cluster)
+    return distances
+
+
+
+def get_intercluster_distances(z,z_occam,leave_out=True,n_random = 200):
+    """Measures intercluster distances (between stars in a cluster and stars from the field) after fitting and transforming
+    INPUTS
+    ------
+    z: apoNN.vector.Vector
+        A vector containing the field star dataset
+    z_occam: vector.OccamLatentVector
+        A vector containing the occam cluster stars
+    leave_out: Boolean
+        True corresponds to excluding clusters being evaluated from training so as to avoid overfitting 
+    n_random: Number of field stars each cluste star is compared too.
+    """
+    distances = []
+    for cluster in list(z_occam.registry.keys()):
+        if leave_out is True:
+            fitter = vector.Fitter(z,z_occam.without(cluster))
+        else:
+            fitter = vector.Fitter(z,z_occam)
+        v_centered_occam = fitter.transform(z_occam.centered.only(cluster))
+        v = fitter.transform(fitter.z.centered)
+        n_v = len(v)
+        distances_cluster = []
+        for idx in np.arange(len(v_centered_occam)):
+            for _ in np.arange(n_random):
+                random_idx = random.randint(0,n_v-1)
+                distances_cluster.append(np.linalg.norm(v_centered_occam[idx]-v[random_idx]))
+
+        distances.append(distances_cluster)
+    return distances
 
