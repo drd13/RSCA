dae58d37e56a3cac36b6323c6130d7c50de589f7
Damien de Mijolla <ddm@splinter-login.local>
2020-11-06 20:04:11 +0000

    modifies vectors such that they can be centered based on an external vector

--- apoNN/src/vectors.py
+++ apoNN/src/vectors.py
@@ -38,9 +38,17 @@ class Vector():
     def raw(self):
         return self._raw
     
-    @property
-    def centered(self):
-        return Vector(self._raw -np.mean(self._raw,axis=0))
+    def centered(self,relative_to=None):
+        """
+        Shift vector to have zero mean.
+        
+        relative_to: vector
+            Vector to use for centering. If relative_to is None then vector is centered using its own mean.
+        """
+        if relative_to is None:
+            return Vector(self._raw -np.mean(self._raw,axis=0))
+        else:
+            return Vector(self._raw -np.mean(relative_to._raw,axis=0))
     
     @property
     def normalized(self):
@@ -110,11 +118,18 @@ class OccamLatentVector(LatentVector,Vector):
             z[cluster_idxs]=self.raw[cluster_idxs]-self.raw[cluster_idxs].mean(axis=0)
         return Vector(z)
 
-
-    @property
-    def centered(self):
-        return OccamLatentVector(self.cluster_names, raw = self._raw -np.mean(self._raw,axis=0))
- 
+    def centered(self,relative_to=None):
+        """
+        Shift vector to have zero mean.
+        
+        relative_to: vector
+            Vector to use for centering. If relative_to is None then vector is centered using its own mean.
+        """
+        if relative_to is None:
+            return OccamLatentVector(self.cluster_names, raw = self._raw -np.mean(self._raw,axis=0))
+        else:
+            return OccamLatentVector(self.cluster_names, raw = self._raw -np.mean(relative_to._raw,axis=0))
+    
 
     def whitened(self,whitener):
         """method that takes a whitening PCA instance and returned a whitened vector"""
@@ -230,14 +245,14 @@ class Fitter():
         self.whitener = PCA(n_components=self.z.raw.shape[1],whiten=True)
         self.pca = PCA(n_components=self.z.raw.shape[1])        
         #make z look like a unit gaussian
-        self.whitener.fit(self.z.centered())
+        self.whitener.fit(self.z.centered().raw)
         #pick-up on directions of z_occam which are the most squashed relative to z
         self.pca.fit(self.z_occam.cluster_centered.whitened(self.whitener)())
         
         #self.scaling_factor = 1 #required to set to 1 because self.transform needs scaling factor
         if use_relative_scaling is True:
             self.scaling_factor = np.std(self.transform(self.z_occam.cluster_centered,scaling=False),axis=0)[None,:]
-            self.scaling_factor[self.scaling_factor>=1]=0.9999 #ensure that dimensions randomly greater than 1 are zeroed out            
+            self.scaling_factor[self.scaling_factor>=1]=0.9999 #ensure that dimensions randomly greater than 1 are zeroed out           
             self.scaling_factor = np.array([self.relative_modifier(std) for std in list(self.scaling_factor[0])])
         else:
             self.scaling_factor = np.std(self.transform(self.z_occam.cluster_centered,scaling=False),axis=0)[None,:]
@@ -252,7 +267,6 @@ class Fitter():
         return transformed_vector
             
     
-    #make-this-a-class-method
     @staticmethod
     def relative_modifier(sigma1,sigma2=1):
             return np.sqrt(np.abs(sigma1**2*sigma2**2/(sigma1**2-sigma2**2)))
