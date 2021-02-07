79657bbf3cf261c46ee040ed176554733b362fa2
Damien de Mijolla <ddm@splinter-login.local>
2021-02-07 14:13:25 +0000

    adds utils function for getting locs of interstellar bands

--- apoNN/src/utils.py
+++ apoNN/src/utils.py
@@ -8,6 +8,19 @@ from pathlib import Path
 import apogee.tools.path as apath
 import apogee.spec.window as apwindow
 
+#def get_interstellar_bands(interstellar_locs = [[750,900],[2400,2450],[2600,2750],[4300,4600]]):
+def get_interstellar_bands(interstellar_locs = [[720,910],[2370,2470],[2600,2750],[4300,4600]]):
+    """returns a mask with one entry for each wavelegnth bin and where locations containing interstellar bands return False    
+    Parameters
+    ----------
+    wavelegth_bins: np.array [[n_start,n_end]]
+        contains start and end values of regions to be masked"""
+    wavelength_grid = np.ones(8575)==1
+    for loc in interstellar_locs:
+        wavelength_grid[loc[0]:loc[1]]=False
+
+    return wavelength_grid,interstellar_locs
+
 
 
 def generate_loss_with_masking(loss):
